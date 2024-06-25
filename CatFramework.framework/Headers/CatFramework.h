fd5c1e57efae104a6e0307119e881c2821e83694
Kostadina Geceska <kostadinag@nextsense.com>
2024-06-25 09:29:38 +0200

    added framework

---
+++ CatFramework.framework/Headers/CatFramework.h
@@ -0,0 +1,18 @@
+//
+//  CatFramework.h
+//  CatFramework
+//
+//  Created by Kostadina Gecevska on 25.6.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CatFramework.
+FOUNDATION_EXPORT double CatFrameworkVersionNumber;
+
+//! Project version string for CatFramework.
+FOUNDATION_EXPORT const unsigned char CatFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CatFramework/PublicHeader.h>
+
+
